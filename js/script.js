f1ee541e65bfc51e255673e8304d0ceea98d187e
Odirlei <odirlei_sabella@hotmail.com>
2023-06-22 16:24:53 -0300

    Responsivo 320px

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ let menu = document.getElementById("menu_links");
 let sombra = document.getElementById("sombra");
 let menu_barras = document.getElementById("menu_barras");
 
+let body = document.getElementsByTagName("body")[0];
+
+addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        menu.style.display = "flex"
+    } else {
+        menu.style.display = "none"
+    }
+});
+
 function mostrarMenu() {
 
     // if (menu.style.left != "10px") {
@@ -10,11 +20,13 @@ function mostrarMenu() {
         sombra.style.right = "-10vw"
         menu_barras.setAttribute("aria-expanded", "true")
         menu_barras.setAttribute("aria-label", "fechar menu")
+        body.style.overflow = "hidden"
     } else {
         menu.style.left = "-300px"
         sombra.style.right = "110vw";
         menu_barras.setAttribute("aria-expanded", "false")
         menu_barras.setAttribute("aria-label", "abrir menu")
+        body.style.overflow = "auto";
     }
     menu_barras.classList.toggle("ativo")
 }
